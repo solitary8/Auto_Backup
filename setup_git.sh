772584f447c086e5601b95a9ba86c2a6352644a5
Edward Watson <117376879+solitary8@users.noreply.github.com>
2024-06-21 17:05:37 +0200

    Update setup_git.sh

--- setup_git.sh
+++ setup_git.sh
@@ -2,9 +2,10 @@
 
 
 sudo apt-get install zenity 
-
+chmod u+x auto_backup_main.sh
 if [[ $? -eq 0 ]]; then
 zenity --info --title="Sucess !" --text="Setup succesfully finished !"
+./auto_backup_main.sh
 else
 	echo "Failed to install packages."
 fi
